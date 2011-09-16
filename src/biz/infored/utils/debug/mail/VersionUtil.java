8a09995fa7a422d1a814ef62f9cb1d97be33e9ba
erling <erling@65f22445-654f-aa4e-9e7f-ca49e9325427>
2011-09-16 19:44:03 +0000

    Initial import.
    git-svn-id: https://svn.infored.no/svn/common/debugMail/trunk@18 65f22445-654f-aa4e-9e7f-ca49e9325427

---
+++ src/biz/infored/utils/debug/mail/VersionUtil.java
@@ -0,0 +1,28 @@
+package biz.infored.utils.debug.mail;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * @author <a href="mailto:erling.molde@infored.no">Erling Molde</a>
+ */
+class VersionUtil
+{
+	private static Pattern	versionPattern	= Pattern.compile("[\\d.]+");
+
+	public static String get(final String source)
+	{
+		if (source == null)
+			return null;
+		final String found = getAll(source);
+		return found.startsWith("1.") ? found.substring(2) : found;
+	}
+
+	public static String getAll(final String source)
+	{
+		if (source == null)
+			return null;
+		final Matcher matcher = versionPattern.matcher(source);
+		return matcher.find() ? matcher.group() : null;
+	}
+}
